335a09fc49e125930f769bc6519ed16831544a81
Michael Alls <allsjr@gmail.com>
2025-04-27 12:45:36 -0400

    turning of signature help

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -33,7 +33,7 @@ return {
 
     -- (Default) Only show the documentation popup when manually triggered
     completion = { documentation = { auto_show = false } },
-    signature = { enabled = true },
+    signature = { enabled = false },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
