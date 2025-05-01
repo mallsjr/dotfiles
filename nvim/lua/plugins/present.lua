09a9ddebd6d36a500c4678a46b42b66359ad0e12
Michael Alls <allsjr@gmail.com>
2025-05-01 09:20:59 -0400

    pulling present.nvim tutorial plugin from github and adding keymap to start

--- nvim/lua/plugins/present.lua
+++ nvim/lua/plugins/present.lua
@@ -1,6 +1,22 @@
 return {
-  dir = "~/plugins/present.nvim/",
+  -- Local development
+  -- dir = "~/plugins/present.nvim/",
+  -- config = function()
+  --   require("present")
+  -- end,
+  "mallsjr/present.nvim",
   config = function()
     require("present")
   end,
+  keys = {
+    {
+      "<leader>sp",
+      function()
+        local buffer_number = vim.api.nvim_get_current_buf()
+        print(buffer_number)
+        require("present").start_presentation({ bufnr = buffer_number })
+      end,
+      desc = "start presentation",
+    },
+  },
 }
