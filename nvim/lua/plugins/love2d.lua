c2070ee971f8149ac9385f01aeb9d737e10b00b9
Michael Alls <allsjr@gmail.com>
2025-05-07 13:43:24 -0400

    updating LSP from having 2 instances running and not understanding love

--- nvim/lua/plugins/love2d.lua
+++ nvim/lua/plugins/love2d.lua
@@ -1,10 +1,8 @@
 return {
   "S1M0N38/love2d.nvim",
-  ft = { "lua" },
-  -- cmd = "LoveRun",
+  cmd = "LoveRun",
   opts = {},
   keys = {
-    { "<leader>l", ft = "lua", desc = "LÖVE" },
     { "<leader>vv", "<cmd>LoveRun<cr>", ft = "lua", desc = "Run LÖVE" },
     { "<leader>vs", "<cmd>LoveStop<cr>", ft = "lua", desc = "Stop LÖVE" },
   },
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -20,6 +20,7 @@ return {
           -- See the configuration section for more details
           -- Load luvit types when the `vim.uv` word is found
           { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          { path = "${3rd}/love2d/library", words = { "love" } },
         },
       },
     },
