af78417110277c4b6cc9c769e5b0184a5d11d099
Michael Alls <allsjr@gmail.com>
2025-06-17 12:44:08 -0400

    adding move up and down keymaps for normal mode

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -11,7 +11,9 @@ vim.keymap.set("n", "<esc><esc>", "<cmd>noh<cr>", { noremap = true, silent = tru
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "moves lines down in visual selection" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "moves lines up in visual selection" })
 
-vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "J", "mz:m+<CR>`z==", { desc = "moves lines down in visual selection" })
+vim.keymap.set("n", "K", "mz:m-2<CR>`z==", { desc = "moves lines up in visual selection" })
+
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "move down in buffer with cursor centered" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "move up in buffer with cursor centered" })
 vim.keymap.set("n", "n", "nzzzv")
